db6706388f242a66690820e7c5e91c4d2f01e54e
Mikhail Zainutdinov <michaelzainutdinov@ya.ru>
2025-04-04 15:21:15 +0300

    Deleted shared_variable

--- zoinks/macros.py
+++ zoinks/macros.py
@@ -27,19 +27,3 @@ def guards_variable(var_name):
         return wrapper
 
     return decorator
-
-
-# TODO: Working on
-def shared_variable(var_name):
-    def decorator(func):
-        if not hasattr(func, '_shared_variables'):
-            func._shared_variables = set()
-        func._shared_variables.add(var_name)
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
